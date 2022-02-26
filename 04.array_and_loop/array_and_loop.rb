57e0b140f4ca2b2eb271ea9c49ff92d99a049a7b
KeishiNoguchi0702 <thomastyaso1127@gmail.com>
2022-02-26 16:25:18 +0900

    throwとcatchを使った大域脱出(タグにはシンボルを記述する)

--- 04.array_and_loop/array_and_loop.rb
+++ 04.array_and_loop/array_and_loop.rb
@@ -656,3 +656,20 @@ end
       break if n == 3
     end
   end
+#
+
+#throwとcatchを使った大域脱出
+
+  #throwとcatchを使った大域脱出(タグにはシンボルを記述する)
+  fruits = %w(apple melon orange)
+  numbers = [1,2,3]
+  catch :done do
+    fruits.shuffle.each do |fruit|
+      numbers.shuffle.each do |n|
+        puts "#{fruit}, #{n}"
+        if fruit == "orange" && n == 3
+          throw :done
+        end
+      end
+    end
+  end
