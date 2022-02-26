4d91f4276a319da03922f1dcbaad8466e4dc7afa
KeishiNoguchi0702 <thomastyaso1127@gmail.com>
2022-02-26 16:10:11 +0900

    shuffleメソッドで配列の要素をランダムに並び替える

--- 04.array_and_loop/array_and_loop.rb
+++ 04.array_and_loop/array_and_loop.rb
@@ -620,3 +620,12 @@ end
   Enumerator.include?(Enumerable) #=> true
 #
 
+#繰り返し諸利用の制御構造
+
+  #break
+  #shuffleメソッドで配列の要素をランダムに並び替える
+  numbers = [1,2,3,4,5].shuffle
+  numbers.each do |n|
+    puts n
+    break if n == 5
+  end
