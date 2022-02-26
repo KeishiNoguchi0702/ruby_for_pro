0d2996d1a042be5ab2cf3a7fb444708acdefcdbf
KeishiNoguchi0702 <thomastyaso1127@gmail.com>
2022-02-26 17:46:26 +0900

    ブロック引数を1つにするとキーと値が配列に格納される

--- 05.hash_and_simbol.rb
+++ 05.hash_and_simbol.rb
@@ -55,3 +55,14 @@
   #japan:yen
   #us:dollar
   #India:ruppe
+
+  #ブロック引数を1つにするとキーと値が配列に格納される
+  currencies = {"japan" => "yen", "us" => "dollar", "India" => "ruppe"}
+  currencies.each do |key_value|
+    key = key_value[0]
+    value = key_value[1]
+    puts "#{key}, #{value}"
+  end
+  #japan, yen
+  #us, dollar
+  #India, ruppe
