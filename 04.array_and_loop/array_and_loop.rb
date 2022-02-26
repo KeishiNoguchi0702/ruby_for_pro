66d19cabd38f67ebcf9e24d071fd0f898f325476
KeishiNoguchi0702 <thomastyaso1127@gmail.com>
2022-02-26 16:12:11 +0900

    while文でbreakによりループを脱出する

--- 04.array_and_loop/array_and_loop.rb
+++ 04.array_and_loop/array_and_loop.rb
@@ -629,3 +629,13 @@ end
     puts n
     break if n == 5
   end
+
+  #while文でbreakによりループを脱出する
+  numbers = [1,2,3,4,5]
+  i = 0
+  while i < numbers.size
+     n = numbers[i]
+     puts n
+     break if n == 5
+     i += 1
+  end
