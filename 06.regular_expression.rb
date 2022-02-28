9fa630078fac3c5833bdd66aae319ae299ad3de3
KeishiNoguchi0702 <thomastyaso1127@gmail.com>
2022-02-28 21:11:08 +0900

    scanメソッド

--- 06.regular_expression.rb
+++ 06.regular_expression.rb
@@ -9,4 +9,14 @@
 TEXT
   
   text.scan(/[A-Z][A-Za-z]+/)
+
+  #[補足]scanメソッド：selfに対して patternを繰り返しマッチし、マッチした部分文字列の配列を返す
+  p "foobar".scan(/../)               # => ["fo", "ob", "ar"]
+  p "foobar".scan("o")                # => ["o", "o"]
+  p "foobarbazfoobarbaz".scan(/ba./)  # => ["bar", "baz", "bar", "baz"]
+
+  p "foobar".scan(/(.)/) # => [["f"], ["o"], ["o"], ["b"], ["a"], ["r"]]
+
+  p "foobarbazfoobarbaz".scan(/(ba)(.)/) # => [["ba", "r"], ["ba", "z"], ["ba", "r"], ["ba", "z"]]
 #
+
